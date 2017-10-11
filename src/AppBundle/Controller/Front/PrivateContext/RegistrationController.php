10554b3bab8bc69497e1dffcd8e0a9d89196e329
skwi <cedric.spalvieri@gmail.com>
2017-10-11 11:08:03 +0200

    Setup Controller structure

---
+++ src/AppBundle/Controller/Front/PrivateContext/RegistrationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller\Front\PrivateContext;
+
+use AppBundle\Controller\Front\BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegistrationController extends BaseController
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function vehicleRegistrationAction(Request $request): Response
+    {
+        return new Response('Oh hai ! Cé koi ta voiture ?');
+    }
+}
