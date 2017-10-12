08fb5632251327b38bdea1fb71efe9bb1e263b9d
stephane <stephane@novaway.fr>
2017-10-12 10:38:54 +0200

    Mise en place de la config front (webpack, tousa) et des sources private

---
+++ app/Resources/build/config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+
+
+// Global configuration
+// - - - - - - - - - - - -
+
+module.exports = {
+  entryApp: [
+    'babel-polyfill',
+    './app/Resources/private/js/app.js',
+    './app/Resources/private/scss/app.scss'
+  ],
+  bundleJS: 'bundle.js',
+  bundleCSS: 'bundle.css',
+  publicPath: path.resolve(__dirname, '../../..', 'web/assets/bundle'),
+  nodeModulesPath: path.resolve(__dirname, '../../..', 'node_modules'),
+  externals: {
+    jquery: 'jQuery'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new WebpackNotifierPlugin({
+      title: 'Wamcar',
+      contentImage: path.join(__dirname, '../../..', 'web/images/favicon/android-chrome-192x192.png'),
+      alwaysNotify: true
+    })
+  ]
+};
