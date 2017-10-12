b8b209172dd13babee99df326c058efb775d5d1d
stephane <stephane@novaway.fr>
2017-10-12 10:38:54 +0200

    Mise en place de la config front (webpack, tousa) et des sources private

---
+++ app/Resources/build/webpack.prod.js
@@ -0,0 +1,72 @@
+var webpack = require('webpack');
+var path = require('path');
+var config = require('./config');
+var ProgressBarPlugin = require('progress-bar-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var postcss = [
+  require('css-mqpacker')({
+    sort: true
+  }),
+  require('autoprefixer')
+];
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+
+// Loaders
+// - - - - - - - - - - - -
+
+config.module.rules = config.module.rules.concat([
+  {
+    test: /\.scss$/,
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            minimize: true
+          }
+        },
+        'postcss-loader',
+        'sass-loader'
+      ]
+    })
+  }
+]);
+
+
+
+// Webpack
+// - - - - - - - - - - - -
+
+module.exports = {
+  entry: {
+    app: config.entryApp
+  },
+  output: {
+    path: config.publicPath,
+    filename: config.bundleJS
+  },
+  module: {
+    rules: config.module.rules,
+  },
+  resolve: config.resolve,
+  plugins: [
+    new ProgressBarPlugin(),
+    new ExtractTextPlugin(config.bundleCSS),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      sourceMap: true
+    }),
+    new WebpackNotifierPlugin({
+      title: 'Wamcar',
+      contentImage: path.join(__dirname, '../../..', 'app/Resources/build/icon-notification.png'),
+      alwaysNotify: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: postcss
+      }
+    })
+  ]
+}
