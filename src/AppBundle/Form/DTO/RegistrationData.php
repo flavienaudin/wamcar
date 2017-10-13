754817b5e476e6d308d91bb78d2c4bacfe7e3abb
Fabien CHOMETTON <fabien@novaway.fr>
2017-10-13 10:57:27 +0200

    fix useless construct

--- src/AppBundle/Form/DTO/RegistrationData.php
+++ src/AppBundle/Form/DTO/RegistrationData.php
@@ -10,13 +10,4 @@ class RegistrationData
     public $email;
     /** @var  string */
     public $password;
-
-    /**
-     * RegistrationData constructor.
-     * @param string|null $email
-     */
-    public function __construct(string $email = null)
-    {
-        $this->email = $email;
-    }
 }
