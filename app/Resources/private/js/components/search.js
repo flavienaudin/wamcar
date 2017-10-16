0bccc123e4a7fcb3793c8e4a2c9940efa914737f
stephane <stephane@novaway.fr>
2017-10-16 17:18:24 +0200

    Intégration des éléments de formulaire

--- app/Resources/private/js/components/search.js
+++ app/Resources/private/js/components/search.js
@@ -9,15 +9,10 @@ import {
 const $searchForm = document.getElementById('js-search-form');
 
 if ($searchForm) {
-
-  /* Scroll */
-
-  const headerHeight = $header.clientHeight;
   const transparentClass = 'is-transparent';
 
   document.addEventListener('scroll', () => {
     const currentScroll = document.documentElement.scrollTop;
-    currentScroll > headerHeight/2 ? $header.classList.remove(transparentClass) : $header.classList.add(transparentClass);
+    currentScroll > 30 ? $header.classList.remove(transparentClass) : $header.classList.add(transparentClass);
   });
-
 }
