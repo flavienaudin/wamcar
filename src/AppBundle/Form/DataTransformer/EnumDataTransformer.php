26c64f46a599dace6d35ea83a2fe4871ceaf819a
skwi <cedric.spalvieri@gmail.com>
2017-10-17 13:44:08 +0200

    Clean DataTransformer/EnumDataTransformer.php file

--- src/AppBundle/Form/DataTransformer/EnumDataTransformer.php
+++ src/AppBundle/Form/DataTransformer/EnumDataTransformer.php
@@ -4,8 +4,6 @@ namespace AppBundle\Form\DataTransformer;
 
 use MyCLabs\Enum\Enum;
 use Symfony\Component\Form\DataTransformerInterface;
-use Symfony\Component\Form\Exception\LogicException;
-use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class EnumDataTransformer implements DataTransformerInterface
 {
@@ -23,16 +21,17 @@ class EnumDataTransformer implements DataTransformerInterface
 
     /**
      * @param mixed $value
-     * @return string
+     * @return null|string
+     * @throws \LogicException when provided data is not an Enum
      */
     public function transform($value): ?string
     {
-        if($value === null) {
+        if ($value === null) {
             return null;
         }
-dump($value);
+
         $value = is_string($value) ? new $this->className($value) : $value;
-dump($value);
+
         if (!$value instanceof Enum) {
             throw new \LogicException(
                 sprintf(
@@ -44,18 +43,6 @@ dump($value);
         }
 
         return $value->getValue();
-//        if (!is_subclass_of($enumClassName, Enum::class)) {
-//            throw new LogicException(sprintf('"%s" should be an instance of "%s" to be transformed into a choice list', $enumClassName, Enum::class));
-//        }
-//
-//        $enumValues = call_user_func([$enumClassName, 'toArray']);
-//
-//        $choices = [];
-//        foreach ($enumValues as $value) {
-//            $choices[$value] = $value;
-//        }
-//
-//        return $choices;
     }
 
     /**
@@ -66,20 +53,4 @@ dump($value);
     {
         return new $this->className($value);
     }
-
-    //    public static function transform(string $enumClassName): array
-    //    {
-    //        if (!is_subclass_of($enumClassName, Enum::class)) {
-    //            throw new LogicException(sprintf('"%s" should be an instance of "%s" to be transformed into a choice list', $enumClassName, Enum::class));
-    //        }
-    //
-    //        $enumValues = call_user_func([$enumClassName, 'toArray']);
-    //
-    //        $choices = [];
-    //        foreach ($enumValues as $value) {
-    //            $choices[$value] = $value;
-    //        }
-    //
-    //        return $choices;
-    //    }
 }
