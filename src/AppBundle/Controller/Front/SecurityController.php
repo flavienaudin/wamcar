ebdb0191b3c974ba0e6ddcebc75de11259410b21
Fabien CHOMETTON <fabien@novaway.fr>
2017-10-19 10:33:28 +0200

    fix conflict rebase

--- src/AppBundle/Controller/Front/SecurityController.php
+++ src/AppBundle/Controller/Front/SecurityController.php
@@ -34,7 +34,7 @@ class SecurityController extends BaseController
      */
     public function registerAction(Request $request): Response
     {
-        $registrationForm = $this->formFactory->create(RegistrationType::class);
+        $registrationForm = $this->formFactory->create(Registration::class);
 
         $registrationForm->handleRequest($request);
 
