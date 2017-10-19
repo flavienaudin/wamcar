d3238931118cbc1230253c004cc655734f1e6be3
stephane <stephane@novaway.fr>
2017-10-19 17:11:59 +0200

    Safari fix

--- app/Resources/private/js/app.js
+++ app/Resources/private/js/app.js
@@ -14,6 +14,7 @@ import { Reveal } from 'foundation-sites/js/foundation.reveal';
 import { Tabs } from 'foundation-sites/js/foundation.tabs';
 import './components/responsiveDom';
 import './components/search';
+import './components/header';
 import './components/select';
 import './components/step';
 import './components/file';
