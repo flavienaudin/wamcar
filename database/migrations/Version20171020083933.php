aad2525e36542deb2383da5bd0ce5f3b7e37c738
Fabien CHOMETTON <fabien@novaway.fr>
2017-10-20 11:27:15 +0200

    fix migration bdd

--- database/migrations/Version20171020083933.php
+++ database/migrations/Version20171020083933.php
@@ -18,7 +18,8 @@ class Version20171020083933 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD profile_title VARCHAR(255) NOT NULL COMMENT \'(DC2Type:Wamcar\\\\User\\\\Title)\', DROP profile_title_value');
+        $this->addSql('ALTER TABLE user DROP profile_title_value');
+        $this->addSql('ALTER TABLE user ADD profile_title VARCHAR(255) DEFAULT NULL COMMENT \'(DC2Type:Wamcar\\\\User\\\\Title)\'');
     }
 
     /**
@@ -29,6 +30,7 @@ class Version20171020083933 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD profile_title_value VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, DROP profile_title');
+        $this->addSql('ALTER TABLE user DROP profile_title');
+        $this->addSql('ALTER TABLE user ADD profile_title_value VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:Wamcar\\\\User\\\\Title)\'');
     }
 }
