1fd1310e1ae82d03d5e845923f34b5bfb50f2224
Fabien CHOMETTON <fabien@novaway.fr>
2017-10-23 16:12:01 +0200

    add template in registration form

--- src/AppBundle/Form/Type/RegistrationType.php
+++ src/AppBundle/Form/Type/RegistrationType.php
@@ -5,9 +5,11 @@ namespace AppBundle\Form\Type;
 
 use AppBundle\Form\DTO\RegistrationDTO;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,18 +22,34 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'user.field.email.placeholder'
+                ]
+            ])
             ->add('password', RepeatedType::class, [
             'type' => PasswordType::class,
             'invalid_message' => 'registration_data.password.repeat',
             'first_options' => [
-                'label_format' => 'user.field.password.first.label'
+                'label_format' => 'user.field.password.first.label',
+                'attr' => [
+                    'placeholder' => 'user.field.password.first.placeholder'
+                ]
             ],
             'second_options' => [
-                'label_format' => 'user.field.password.second.label'
+                'label_format' => 'user.field.password.second.label',
+                'attr' => [
+                    'placeholder' => 'user.field.password.second.placeholder'
+                ]
             ],
             'required' => true
-        ]);
+            ])
+            ->add('accept', CheckboxType::class, [
+                "mapped" => false,
+            ])
+            ->add('create', SubmitType::class, [
+                'label' => 'user.submit.i_register'
+            ])
         ;
     }
 
