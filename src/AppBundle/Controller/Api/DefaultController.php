e12f64e0faefa74f563bd4454945d5a373445c06
skwi <cedric.spalvieri@gmail.com>
2017-10-30 17:46:25 +0100

    API routing setup

--- src/AppBundle/Controller/Api/DefaultController.php
+++ src/AppBundle/Controller/Api/DefaultController.php
@@ -2,7 +2,17 @@
 
 namespace AppBundle\Controller\Api;
 
-class DefaultController
-{
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+class DefaultController extends BaseController
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function documentationAction(Request $request): Response
+    {
+        die('todo');
+    }
 }
