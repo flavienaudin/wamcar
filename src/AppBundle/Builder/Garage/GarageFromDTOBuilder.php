e4a36a4b7445530e4d674eee681cebc7848ed957
Fabien CHOMETTON <fabien@novaway.fr>
2017-10-31 11:47:04 +0100

    add edit garage

--- src/AppBundle/Builder/Garage/GarageFromDTOBuilder.php
+++ src/AppBundle/Builder/Garage/GarageFromDTOBuilder.php
@@ -3,23 +3,42 @@
 namespace AppBundle\Builder\Garage;
 
 
+use AppBundle\Builder\BuilderFromDTO;
 use AppBundle\Doctrine\Entity\ApplicationGarage;
-use AppBundle\DTO\BuilderFromDTO;
+use AppBundle\Doctrine\Repository\DoctrineGarageRepository;
 use AppBundle\Form\DTO\GarageDTO;
 use Wamcar\Garage\Garage;
 
 class GarageFromDTOBuilder implements BuilderFromDTO
 {
+    /**
+     * @var DoctrineGarageRepository $garageRepository
+     */
+    private $garageRepository;
 
     /**
-     * @param mixed $dto
+     * GarageFromDTOBuilder constructor.
+     * @param DoctrineGarageRepository $garageRepository
+     */
+    public function __construct(
+        DoctrineGarageRepository $garageRepository
+    )
+    {
+        $this->garageRepository = $garageRepository;
+    }
+
+    /**
+     * @param GarageDTO $dto
      * @return Garage
      * @throws \InvalidArgumentException
      * @throws \UnexpectedValueException
      */
     public function buildFromDTO($dto): Garage
     {
-        return $this->buildNewGarageFromDto($dto);
+        if (null === $dto->id)
+            return $this->buildNewGarageFromDto($dto);
+        else
+            return $this->buildEditGarageFromDto($dto);
     }
 
     /**
@@ -49,6 +68,36 @@ class GarageFromDTOBuilder implements BuilderFromDTO
             $dto->benefit,
             $dto->fillAddress()
         );
+    }
+
+    /**
+     * Create a new Garage from dto
+     *
+     * @param $dto
+     * @return Garage
+     */
+    protected function buildEditGarageFromDto($dto): Garage
+    {
+        if (!$dto instanceof GarageDTO) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    "GarageFromDTOBuilder::buildFromDTO expects $dto argument to be an instance of '%s', '%s' given"),
+                GarageDTO::class,
+                get_class($dto)
+            );
+        }
+
+        $applicationGarage = $this->garageRepository->findOne($dto->id);
+        $applicationGarage->setEmail($dto->email);
+        $applicationGarage->setName($dto->name);
+        $applicationGarage->setPhone($dto->phone);
+        $applicationGarage->setSiren($dto->siren);
+        $applicationGarage->setOpeningHours($dto->openingHours);
+        $applicationGarage->setPresentation($dto->presentation);
+        $applicationGarage->setBenefit($dto->benefit);
+        $applicationGarage->setAddress($dto->fillAddress());
+
+        return $applicationGarage;
     }
 
 }
