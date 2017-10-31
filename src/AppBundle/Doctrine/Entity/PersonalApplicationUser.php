e3a23462b584768b69b6854168a6c2295d04b7bf
Fabien CHOMETTON <fabien@novaway.fr>
2017-10-31 16:06:20 +0100

    add role on proUser

--- src/AppBundle/Doctrine/Entity/PersonalApplicationUser.php
+++ src/AppBundle/Doctrine/Entity/PersonalApplicationUser.php
@@ -42,4 +42,12 @@ class PersonalApplicationUser extends PersonalUser implements \Serializable, Sho
     {
         return $this->registrationToken === null;
     }
+
+    /**
+     * @return array
+     */
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
 }
--- src/AppBundle/Doctrine/Entity/ProApplicationUser.php
+++ src/AppBundle/Doctrine/Entity/ProApplicationUser.php
@@ -14,22 +14,28 @@ class ProApplicationUser extends ProUser implements \Serializable, ApplicationUs
     use PasswordResettableTrait;
     use ApiCredentialTrait;
 
+    /** @var  array */
+    protected $roles;
+
     /**
      * ApplicationUser constructor.
      * @param string $email
      * @param string $password
      * @param string $salt
+     * @param string $roles
      */
     public function __construct(
         string $email,
         string $password,
-        string $salt
+        string $salt,
+        string $roles = null
     )
     {
         parent::__construct($email);
 
         $this->password = $password;
         $this->salt = $salt;
+        $this->roles = $roles ? [$roles] : ['ROLE_PRO'];
         $this->generateApiCredentials();
     }
 
@@ -39,5 +45,13 @@ class ProApplicationUser extends ProUser implements \Serializable, ApplicationUs
     public function hasConfirmedRegistration(): bool
     {
         return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
     }
 }
