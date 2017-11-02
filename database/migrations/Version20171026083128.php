3a032f3d28284f1370f11bd6a587b714a6ae27f4
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-02 11:22:45 +0100

    add migrations comment

--- database/migrations/Version20171026083128.php
+++ database/migrations/Version20171026083128.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Add field specific to proUser
  */
 class Version20171026083128 extends AbstractMigration
 {
--- database/migrations/Version20171026141459.php
+++ database/migrations/Version20171026141459.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Add password_reset_token
  */
 class Version20171026141459 extends AbstractMigration
 {
