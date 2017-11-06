c9bf3f2bcfcb61a2bd4da46d6db50bf5d6dcfdb3
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-06 15:59:32 +0100

    add softdeleteable on garage + applicationGarage

--- src/AppBundle/Doctrine/Entity/ApplicationGarage.php
+++ src/AppBundle/Doctrine/Entity/ApplicationGarage.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Doctrine\Entity;
 
 use Gedmo\SoftDeleteable\Traits\SoftDeleteable;
-use Wamcar\Garage\Address;
 use Wamcar\Garage\Garage;
 
 class ApplicationGarage extends Garage
@@ -13,30 +12,4 @@ class ApplicationGarage extends Garage
     /** @var string */
     protected $deletedAt;
 
-
-    /**
-     * ApplicationGarage constructor.
-     * @param string $name
-     * @param string $siren
-     * @param string|null $openingHours
-     * @param string|null $presentation
-     * @param Address $address
-     */
-    public function __construct(
-        string $name,
-        string $siren,
-        string $openingHours = null,
-        string $presentation = null,
-        Address $address
-    )
-    {
-        parent::__construct(
-            $name,
-            $siren,
-            $openingHours,
-            $presentation,
-            $address
-            );
-
-    }
 }
