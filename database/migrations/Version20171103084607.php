69c0a32fffdd1fec197cf2ededa967329a09aaf3
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-07 16:26:39 +0100

    add comment migration

--- database/migrations/Version20171103084607.php
+++ database/migrations/Version20171103084607.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Add garage pro user
  */
 class Version20171103084607 extends AbstractMigration
 {
