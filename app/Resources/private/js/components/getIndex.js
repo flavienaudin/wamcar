98dc22ef20d799938e3041cc20f92c0dab1dfa03
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-08 10:26:40 +0100

    clean console log

--- app/Resources/private/js/components/getIndex.js
+++ app/Resources/private/js/components/getIndex.js
@@ -6,8 +6,6 @@ export default (node) => {
   const childs = node.parentNode.children;
   let i = 0;
 
-  console.log(childs);
-
   for (i = 0; i < childs.length; i++) {
     if (node == childs[i]) break;
   }
