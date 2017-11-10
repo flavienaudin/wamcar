83804a0f56df613eab08a81a6b4462dcbe5a4090
skwi <cedric.spalvieri@gmail.com>
2017-11-10 09:07:26 +0100

    Add default value for role, for existing Pro users

---
+++ database/migrations/Version20171110080424.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171110080424 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("UPDATE user SET role = 'ROLE_PRO' WHERE discriminator = 'pro'");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
