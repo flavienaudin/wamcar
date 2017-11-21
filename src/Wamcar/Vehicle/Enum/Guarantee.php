c5fc08b68aaaa9b66b77888cea6c2f5430029f73
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-21 17:16:54 +0100

    clean conflict

--- src/Wamcar/Vehicle/Enum/Guarantee.php
+++ src/Wamcar/Vehicle/Enum/Guarantee.php
@@ -11,15 +11,9 @@ use MyCLabs\Enum\Enum;
  */
 final class Guarantee extends Enum
 {
-<<<<<<< 42b41ee91edb9dde149232d7288d28b5bc144e95
     const G12_MONTH = '12_month';
     const G24_MONTH = '24_month';
     const G36_MONTH = '36_month';
-=======
-    const GUARANTEE_12_MONTH = 'guarantee_12_month';
-    const GUARANTEE_24_MONTH = 'guarantee_24_month';
-    const GUARANTEE_36_MONTH = 'guarantee_36_month';
->>>>>>> add pro vehicle with offer
 }
 
 
