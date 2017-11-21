b78bf548d7fd61a41dd654d2c3dd9431486f8241
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-21 17:27:26 +0100

    refacto clic complete platenumber

--- app/Resources/private/js/components/vehicle.js
+++ app/Resources/private/js/components/vehicle.js
@@ -3,10 +3,8 @@ const $plateField = document.querySelector('#pro_vehicle_registrationNumber');
 const $sectionRegistration = document.querySelector('#js-registration-information');
 const $linkPlate = document.querySelector('#js-update-plate');
 
-$plateField.addEventListener('keyup', () => {
-  if ($plateField.value.length > 4) {
-      let link = $sectionRegistration.getAttribute('data-fetch-plate');
-      let completeLink = link.replace('_plate_number_', $plateField.value);
-      $linkPlate.href= completeLink;
-  }
+$linkPlate.addEventListener('click', function(event) {
+  event.preventDefault();
+  const linkTemplate = $sectionRegistration.getAttribute('data-fetch-plate');
+  window.location = linkTemplate.replace('_plate_number_', $plateField.value);
 });
