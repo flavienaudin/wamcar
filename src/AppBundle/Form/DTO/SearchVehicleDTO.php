1a23a1300c3e708041f0d153fd67260464eb798d
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-29 14:24:44 +0100

    clean

--- src/AppBundle/Form/DTO/SearchVehicleDTO.php
+++ src/AppBundle/Form/DTO/SearchVehicleDTO.php
@@ -8,8 +8,6 @@ use Novaway\ElasticsearchClient\Filter\TermFilter;
 class SearchVehicleDTO
 {
     /** @var string */
-    public $queryTerm;
-    /** @var string */
     public $text;
     /** @var string */
     public $postalCode;
@@ -19,49 +17,5 @@ class SearchVehicleDTO
     public $latitude;
     /** @var string */
     public $longitude;
-    /** @var string */
-    public $sort;
-
-
-    /**
-     * SearchFilterData constructor.
-     * @param string $queryTerm
-     */
-    public function __construct(string $queryTerm = null)
-    {
-        $this->queryTerm = $queryTerm;
-    }
-    /**
-     * @return bool
-     */
-    public function hasQueryTerm(): bool
-    {
-        return null !== $this->queryTerm;
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasFilter(): bool
-    {
-        return null !== $this->text
-            ;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isStorable(): bool
-    {
-        return $this->hasFilter() || $this->hasQueryTerm();
-    }
-
-    /**
-     * @return string
-     */
-    public function getSort()
-    {
-        return $this->sort;
-    }
 
 }
