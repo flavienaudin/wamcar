dd9729bd29b797d3028cc8638aca5a088b10bfa1
Fabien CHOMETTON <fabien@novaway.fr>
2017-11-30 16:07:57 +0100

    preparation complete form

--- src/AppBundle/Form/Type/SearchVehicleType.php
+++ src/AppBundle/Form/Type/SearchVehicleType.php
@@ -3,11 +3,14 @@
 namespace AppBundle\Form\Type;
 
 use AppBundle\Form\DTO\SearchVehicleDTO;
+use AppBundle\Utils\YearsChoice;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Wamcar\Vehicle\Enum\Transmission;
 
 class SearchVehicleType extends AbstractType
 {
@@ -30,6 +33,58 @@ class SearchVehicleType extends AbstractType
             ])
             ->add('longitude', HiddenType::class, [
                 'required' => false
+            ])
+            ->add('make', ChoiceType::class, [
+                'choices' => $availableValues['make'] ?? [],
+                'placeholder' => count($availableValues['make'] ?? []) === 1 ? false : '',
+                'error_bubbling' => true,
+            ])
+            ->add('model', ChoiceType::class, [
+                'choices' => $availableValues['model'] ?? [],
+                'placeholder' => count($availableValues['model'] ?? []) === 1 ? false : '',
+                'error_bubbling' => true,
+            ])
+            ->add('transmission', ChoiceType::class, [
+                'choices' => Transmission::toArray(),
+                'error_bubbling' => true,
+            ])
+            ->add('fuel', ChoiceType::class, [
+                'choices' => $availableValues['fuel'] ?? [],
+                'placeholder' => count($availableValues['fuel'] ?? []) === 1 ? false : '',
+                'error_bubbling' => true,
+            ])
+            ->add('mileageMax', ChoiceType::class, [
+                'choices' => [
+                    '50 000 Km' => '50000',
+                    '100 000 Km' => '100000',
+                    '150 000 Km' => '150000',
+                    '200 000 Km' => '200000'
+                ],
+                'error_bubbling' => true,
+            ])
+            ->add('yearsMin', ChoiceType::class, [
+                'choices' => YearsChoice::getLastYears(),
+                'error_bubbling' => true,
+            ])
+            ->add('yearsMax', ChoiceType::class, [
+                'choices' => YearsChoice::getLastYears(),
+                'error_bubbling' => true,
+            ])
+            ->add('budgetMin', ChoiceType::class, [
+                'choices' => [
+                    '10 000 €' => '10000',
+                    '20 000 €' => '20000',
+                    '30 000 €' => '30000'
+                ],
+                'error_bubbling' => true,
+            ])
+            ->add('budgetMax', ChoiceType::class, [
+                'choices' => [
+                    '10 000 €' => '10000',
+                    '20 000 €' => '20000',
+                    '30 000 €' => '30000'
+                ],
+                'error_bubbling' => true,
             ])
         ;
     }
---
+++ src/AppBundle/Utils/YearsChoice.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace AppBundle\Utils;
+
+
+class YearsChoice
+{
+    /**
+     * Return a list of last years
+     *
+     * @return array
+     */
+    public static function getLastYears($nbYears = 7)
+    {
+        $arrayYear = [];
+        $currentYear = date('Y');
+        for ($i = $nbYears; $i >=0; $i--) {
+            $arrayYear[$currentYear - $i] = $i;
+        }
+        asort($arrayYear);
+
+        return $arrayYear;
+    }
+}
