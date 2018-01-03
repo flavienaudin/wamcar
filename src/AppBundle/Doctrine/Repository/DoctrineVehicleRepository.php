00d2cf121b86feaea255afc92b592f85bca6e1e0
flavien audin <flavien.audin@gmail.com>
2018-01-03 11:44:13 +0100

    Suppression d'un véhicule dans la fiche pro

--- src/AppBundle/Doctrine/Repository/DoctrineVehicleRepository.php
+++ src/AppBundle/Doctrine/Repository/DoctrineVehicleRepository.php
@@ -32,7 +32,8 @@ class DoctrineVehicleRepository extends EntityRepository
      */
     public function remove(Vehicle $vehicle): void
     {
-        throw new \LogicException("Not implemented");
+        $this->_em->persist($vehicle);
+        $this->_em->flush();
     }
 
     /**
