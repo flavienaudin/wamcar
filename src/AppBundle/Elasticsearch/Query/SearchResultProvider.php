67bcd09dc395ff9cd75881e5f241b522d3b84bf7
Fabien CHOMETTON <fabien@novaway.fr>
2018-01-12 10:35:29 +0100

    refacto searchResultProvider

--- src/AppBundle/Elasticsearch/Query/SearchResultProvider.php
+++ src/AppBundle/Elasticsearch/Query/SearchResultProvider.php
@@ -6,6 +6,7 @@ namespace AppBundle\Elasticsearch\Query;
 
 use AppBundle\Controller\Front\ProContext\SearchController;
 use AppBundle\Elasticsearch\Type\IndexablePersonalVehicle;
+use AppBundle\Form\DTO\SearchVehicleDTO;
 use Novaway\ElasticsearchClient\Query\QueryBuilder;
 use Novaway\ElasticsearchClient\QueryExecutor;
 
@@ -40,74 +41,37 @@ class SearchResultProvider
     {
         $searchVehicleDTO = $searchForm->getData();
 
-        $queryBuilderBase = $this->initializeQueryBuilder($pages);
-        $queryBuilder = [];
-
-        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-            foreach ($this->queryTypes as $queryType) {
-                $queryBuilder[$queryType] = $this->queryBuilderFilterer->getQueryBuilder($queryBuilderBase[$queryType], $searchVehicleDTO, $queryType);
-            }
-        } else {
-            foreach ($this->queryTypes as $queryType) {
-                $queryBuilder[$queryType] = $queryBuilderBase[$queryType];
-            }
-        }
-
-        $queryBody = $this->getQueryBody($queryBuilder);
-
-        return $this->getQueryResult($queryBody);
-    }
-
-    /**
-     * @param $pages
-     * @return array|QueryBuilder[]
-     */
-    private function initializeQueryBuilder($pages)
-    {
-        $queryBuilderBase = [];
-
-        foreach ($this->queryTypes as $queryType) {
-            $queryBuilderBase[$queryType] = QueryBuilder::createNew(
-                self::OFFSET + ($pages[$queryType] - 1) * self::LIMIT,
-                self::LIMIT,
-                self::MIN_SCORE
-            );
-        }
-
-        return $queryBuilderBase;
-    }
-
-    /**
-     * @param $queryBuilder
-     * @return array
-     */
-    private function getQueryBody($queryBuilder)
-    {
-        $queryBody = [];
-
-        foreach ($this->queryTypes as $queryType) {
-            $queryBody[$queryType] = $queryBuilder[$queryType]->getQueryBody();
-        }
-
-        return $queryBody;
-    }
-
-    /**
-     * @param $queryBody
-     * @return array
-     */
-    private function getQueryResult($queryBody)
-    {
         $searchResult = [];
-
         foreach ($this->queryTypes as $queryType) {
-            $searchResult[$queryType] = $this->queryExecutor->execute(
-                $queryBody[$queryType],
-                IndexablePersonalVehicle::TYPE
-            );
+            $searchResult[$queryType] = $this->getQueryResult($queryType, $searchVehicleDTO, $pages, $searchForm->isSubmitted() && $searchForm->isValid());
         }
 
         return $searchResult;
     }
 
+    /**
+     * @param string $queryType
+     * @param SearchVehicleDTO $searchVehicleDTO
+     * @param array $pages
+     * @param bool $submittedAndValid
+     * @return \Novaway\ElasticsearchClient\Query\Result
+     */
+    private function getQueryResult(string $queryType, SearchVehicleDTO $searchVehicleDTO, array $pages, bool $submittedAndValid)
+    {
+
+        $queryBuilder = QueryBuilder::createNew(
+            self::OFFSET + ($pages[$queryType] - 1) * self::LIMIT,
+            self::LIMIT,
+            self::MIN_SCORE
+        );
+
+        if ($submittedAndValid) {
+            $queryBuilder = $this->queryBuilderFilterer->getQueryBuilder($queryBuilder, $searchVehicleDTO, $queryType);
+        }
+
+        return $this->queryExecutor->execute(
+            $queryBuilder->getQueryBody(),
+            IndexablePersonalVehicle::TYPE
+        );
+    }
 }
