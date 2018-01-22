39e10c1ba03b119f07986e60a46610a2d913f8d1
skwi <cedric.spalvieri@gmail.com>
2018-01-22 16:12:29 +0100

    Update API documentation

---
+++ src/AppBundle/Api/DTO/VehicleShortDTO.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Api\DTO;
+use AppBundle\Services\Vehicle\CanBeProVehicle;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Wamcar\Vehicle\ProVehicle;
+use Wamcar\Vehicle\Vehicle;
+
+/**
+ * @SWG\Definition(
+ *   definition="VehicleShort",
+ *   type="object"
+ * )
+ */
+final class VehicleShortDTO
+{
+    /** @SWG\Property(type="integer", format="int64") */
+    public $id;
+    /** @SWG\Property(type="string", format="date") */
+    public $updatedDate;
+
+    /**
+     * @param ProVehicle $proVehicle
+     * @return VehicleShortDTO
+     */
+    public static function createFromProVehicle(ProVehicle $proVehicle): self
+    {
+        try {
+            $vehicleDto = new self();
+
+            $vehicleDto->id = $proVehicle->getReference();
+            $vehicleDto->updatedDate = $proVehicle->getUpdatedAt();
+
+            return $vehicleDto;
+        }
+        catch(\Exception $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+    }
+}
