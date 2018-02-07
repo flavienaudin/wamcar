5e80583271001dfafece38990f0f37dff0980e86
Fabien CHOMETTON <fabien@novaway.fr>
2018-02-07 12:25:56 +0100

    start implementation

--- src/AppBundle/Controller/Front/ProContext/ConversationController.php
+++ src/AppBundle/Controller/Front/ProContext/ConversationController.php
@@ -9,10 +9,13 @@ use AppBundle\Form\DTO\MessageDTO;
 use AppBundle\Form\Type\MessageType;
 use AppBundle\Services\Conversation\ConversationAuthorizationChecker;
 use AppBundle\Services\Conversation\ConversationEditionService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Wamcar\User\BaseUser;
+use Wamcar\Vehicle\BaseVehicle;
+use Wamcar\Vehicle\Vehicle;
 
 class ConversationController extends BaseController
 {
@@ -52,18 +55,23 @@ class ConversationController extends BaseController
         ]);
     }
 
-
     /**
+     * @ParamConverter("interlocutor", options={"id" = "user_id"})
+     * @ParamConverter("vehicle", options={"id" = "vehicle_id"})
      * @param Request $request
      * @param BaseUser $interlocutor
+     * @param null|BaseVehicle $vehicle
      * @return Response
      */
-    public function createAction(Request $request, BaseUser $interlocutor): Response
+    public function createAction(Request $request, BaseUser $interlocutor, BaseVehicle $vehicle): Response
     {
         $this->conversationAuthorizationChecker->canCommunicate($this->getUser(), $interlocutor);
 
         if ($conversation = $this->conversationEditionService->getConversation($this->getUser(), $interlocutor)) {
-            return $this->redirectToRoute('front_conversation_edit', ['id' => $conversation->getId()]);
+            if ($vehicle) {
+                return $this->redirectToRoute('front_conversation_edit_vehicle', ['conversation_id' => $conversation->getId(), 'vehicle_id' => $vehicle->getId()]);
+            }
+            return $this->redirectToRoute('front_conversation_edit', ['conversation_id' => $conversation->getId()]);
         }
 
         $messageDTO = new MessageDTO(null, $this->getUser(), $interlocutor);
@@ -72,11 +80,14 @@ class ConversationController extends BaseController
     }
 
     /**
+     * @ParamConverter("conversation", options={"id" = "conversation_id"})
+     * @ParamConverter("vehicle", options={"id" = "vehicle_id"})
      * @param Request $request
      * @param ApplicationConversation $conversation
+     * @param null|BaseVehicle $vehicle
      * @return Response
      */
-    public function editAction(Request $request, ApplicationConversation $conversation): Response
+    public function editAction(Request $request, ApplicationConversation $conversation, ?BaseVehicle $vehicle = null): Response
     {
         $messageDTO = MessageDTO::buildFromConversation($conversation, $this->getUser());
         $this->conversationEditionService->updateLastOpenedAt($conversation, $this->getUser());
