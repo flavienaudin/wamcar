2b71ff6c8fc3e7c5dfba7a15b142ca392810ad25
Fabien CHOMETTON <fabien@novaway.fr>
2018-02-13 09:50:05 +0100

    rename id

--- app/Resources/private/js/components/conversation.js
+++ app/Resources/private/js/components/conversation.js
@@ -2,7 +2,7 @@
    Conversation
    =========================================================================== */
 
-let $removeButton = document.getElementById('js-vehicle-selected');
+let $removeButton = document.getElementById('js-remove-vehicle-selected');
 
 if ($removeButton) {
   let $input = document.getElementById('message_vehicle');
