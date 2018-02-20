a201ec51d57c21c4da0ffd4c42c0fa21ae36c9a9
Fabien CHOMETTON <fabien@novaway.fr>
2018-02-20 18:01:59 +0100

    refacto clearSelect function

--- app/Resources/private/js/components/select.js
+++ app/Resources/private/js/components/select.js
@@ -5,6 +5,9 @@
 import {
   disabledClass
 } from '../settings/settings.js';
+import {
+  clearSelectOptions
+} from './registration';
 
 const $toggleId = document.querySelectorAll('[data-toggle-id]');
 
@@ -12,25 +15,13 @@ const $toggleId = document.querySelectorAll('[data-toggle-id]');
   const id = select.getAttribute('data-toggle-id');
   const dataFetchUrl = select.getAttribute('data-fetch-url');
 
-  let clearSelect = function (select, doAddEmpty) {
-    $(select).find('option').remove();
-
-    if (!doAddEmpty) {
-      return;
-    }
-
-    let defaultOption = document.createElement('option');
-    defaultOption.text = '';
-    select.add(defaultOption);
-  };
-
   select.addEventListener('change', () => {
     let value = select.value;
     let target = document.getElementById(id);
     target.classList.add(disabledClass);
 
     let targetSelect = target.getElementsByTagName('select')[0];
-    clearSelect(targetSelect);
+    clearSelectOptions(targetSelect);
 
     fetch(dataFetchUrl.replace('%value%', select.value), {
       credentials: 'include',
