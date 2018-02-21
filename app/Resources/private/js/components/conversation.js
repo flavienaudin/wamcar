24bad7de18305a3bad61292768a910ff86b5e589
Fabien CHOMETTON <fabien@novaway.fr>
2018-02-21 16:30:24 +0100

    prevent change in messagerie unless submit form

--- app/Resources/private/js/components/conversation.js
+++ app/Resources/private/js/components/conversation.js
@@ -3,6 +3,7 @@
    =========================================================================== */
 
 let $removeButton = document.getElementById('js-remove-vehicle-selected');
+let $inputText = document.getElementById('message_content');
 
 if ($removeButton) {
   let $input = document.getElementById('message_vehicle');
@@ -11,5 +12,19 @@ if ($removeButton) {
     $input.value = '';
     const preview = $removeButton.closest('.messages-item-vehicle-taken');
     preview.remove();
+  });
+}
+
+if ($inputText) {
+  window.addEventListener('beforeunload', function (e) {
+    let srcUnloadEvent = document.activeElement;
+    let unlockRefresh = srcUnloadEvent.hasAttribute('type') && srcUnloadEvent.getAttribute('type') === 'submit';
+
+    if ($inputText.value !== '' && !unlockRefresh) {
+      var confirmationMessage = 'Do you really want to leave our brilliant application?';
+
+      e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
+      return confirmationMessage;              // Gecko, WebKit, Chrome <34
+    }
   });
 }
