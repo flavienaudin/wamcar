5efa7bbaf783d6db4d74e2eb9c64097a46e845a0
Flavien <flavien.audin@gmail.com>
2018-03-21 15:19:11 +0100

    Menu : affichage de l'avatar de l'utilisateur et de son prénom
    Fix : disparition du menu de navigation dans le offcanvas, si non connecté et modification de la largeur de la fenêtre

--- app/Resources/private/js/components/responsiveDom.js
+++ app/Resources/private/js/components/responsiveDom.js
@@ -10,10 +10,7 @@ const $offCanvasNavigation = '#js-off-canvas-navigation';
 
 $($navigation).responsiveDom({
   appendTo: $offCanvasNavigation,
-  mediaQuery: '(max-width: 1023px)',
-  callback: (matched) => {
-    matched && $($navigation).toggleClass('show-for-large');
-  }
+  mediaQuery: '(max-width: 1023px)'
 });
 
 const $movePicturesList = '#js-move-pictures-list';
