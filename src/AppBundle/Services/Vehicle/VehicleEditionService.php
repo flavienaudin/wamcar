65365b1a5bf0e63e6f6709e7a5e6a7c47df4122a
Flavien Audin <flavien.audin@gmail.com>
2018-04-10 12:00:07 +0200

    Watermark : images des véhicules
    Ajout du filtre "Watermark" aux ensembles de filtres : vehicle_picture, vehicle_mini_thumbnail, vehicle_thumbnail
    Génération automatiques des caches (images avec les filtres) lors de l'ajout et la mise à jour des véhicules.

--- src/AppBundle/Services/Vehicle/VehicleEditionService.php
+++ src/AppBundle/Services/Vehicle/VehicleEditionService.php
@@ -2,22 +2,9 @@
 
 namespace AppBundle\Services\Vehicle;
 
-use AppBundle\Api\DTO\VehicleDTO as ApiVehicleDTO;
-use AppBundle\Api\EntityBuilder\ProVehicleBuilder as ApiVehicleBuilder;
-use AppBundle\Doctrine\Entity\ProVehiclePicture;
-use AppBundle\Doctrine\Repository\DoctrineMessageRepository;
-use AppBundle\Form\DTO\ProVehicleDTO as FormVehicleDTO;
-use AppBundle\Form\EntityBuilder\ProVehicleBuilder as FormVehicleBuilder;
 use Doctrine\ORM\EntityManager;
-use SimpleBus\Message\Bus\MessageBus;
 use Wamcar\Conversation\Message;
-use Wamcar\Garage\Garage;
-use Wamcar\Garage\GarageRepository;
 use Wamcar\Vehicle\BaseVehicle;
-use Wamcar\Vehicle\Event\ProVehicleCreated;
-use Wamcar\Vehicle\Event\ProVehicleRemoved;
-use Wamcar\Vehicle\ProVehicle;
-use Wamcar\Vehicle\ProVehicleRepository;
 
 
 class VehicleEditionService
