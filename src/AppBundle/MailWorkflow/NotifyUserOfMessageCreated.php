31a066d2cf3b4f904571dad7969ed4eaf479d4f6
Flavien Audin <flavien.audin@gmail.com>
2018-08-31 11:34:00 +0200

    MailWorkflow : envoi de l'email pour prévenir d'un message privé, uniquement si les préférences du destinataire le permettent

--- src/AppBundle/MailWorkflow/NotifyUserOfMessageCreated.php
+++ src/AppBundle/MailWorkflow/NotifyUserOfMessageCreated.php
@@ -10,6 +10,7 @@ use Wamcar\Conversation\Event\MessageCreated;
 use Wamcar\Conversation\Event\MessageEvent;
 use Wamcar\Conversation\Event\MessageEventHandler;
 use Wamcar\Conversation\Message;
+use Wamcar\Vehicle\Enum\NotificationFrequency;
 use Wamcar\Vehicle\ProVehicle;
 
 class NotifyUserOfMessageCreated extends AbstractEmailEventHandler implements MessageEventHandler
@@ -25,21 +26,24 @@ class NotifyUserOfMessageCreated extends AbstractEmailEventHandler implements Me
         /** @var Message $message */
         $message = $event->getMessage();
         $interlocutor = $event->getInterlocutor();
-        $pathImg = $event->getPathImg();
+        if ($interlocutor->getPreferences()->isPrivateMessageEmailEnabled()
+            && $interlocutor->getPreferences()->getPrivateMessageEmailFrequency()->getValue() === NotificationFrequency::IMMEDIATELY) {
+            $pathImg = $event->getPathImg();
 
-        $this->send(
-            $this->translator->trans('notifyUserOfMessageCreated.object', ['%messageAuthorName%' => $message->getUser()->getFullName()], 'email'),
-            'Mail/notifyUserOfMessageCreated.html.twig',
-            [
-                'username' => $interlocutor->getFullName(),
-                'messageAuthorName' => $message->getUser()->getFullName(),
-                'message' => $message->getContent(),
-                'message_url' => $this->router->generate("front_conversation_edit", ['id' => $message->getConversation()->getId(),'_fragment'=>'last-message'], UrlGeneratorInterface::ABSOLUTE_URL),
-                'vehicle' => $message->getVehicle(),
-                'vehiclePrice' => ($message->getVehicle() instanceof ProVehicle?$message->getVehicle()->getPrice():null),
-                'thumbnailUrl' => $pathImg
-            ],
-            new EmailRecipientList([$this->createUserEmailContact($interlocutor)])
-        );
+            $this->send(
+                $this->translator->trans('notifyUserOfMessageCreated.object', ['%messageAuthorName%' => $message->getUser()->getFullName()], 'email'),
+                'Mail/notifyUserOfMessageCreated.html.twig',
+                [
+                    'username' => $interlocutor->getFullName(),
+                    'messageAuthorName' => $message->getUser()->getFullName(),
+                    'message' => $message->getContent(),
+                    'message_url' => $this->router->generate("front_conversation_edit", ['id' => $message->getConversation()->getId(), '_fragment' => 'last-message'], UrlGeneratorInterface::ABSOLUTE_URL),
+                    'vehicle' => $message->getVehicle(),
+                    'vehiclePrice' => ($message->getVehicle() instanceof ProVehicle ? $message->getVehicle()->getPrice() : null),
+                    'thumbnailUrl' => $pathImg
+                ],
+                new EmailRecipientList([$this->createUserEmailContact($interlocutor)])
+            );
+        }
     }
 }
