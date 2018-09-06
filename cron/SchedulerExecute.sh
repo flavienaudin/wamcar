3a813361aa310f027f35f76933425d6b7ee0eb53
Flavien Audin <flavien.audin@gmail.com>
2018-09-06 10:21:53 +0200

    Scheduler command : script de lancement. Ajout de l'environnement

--- cron/SchedulerExecute.sh
+++ cron/SchedulerExecute.sh
@@ -3,12 +3,13 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"/..
 
+ENV="${SYMFONY_ENV:-dev}"
+
 if [ "docker-cmd" = $1 ]
 then
-
     export USER_ID=`id -u`
     export USER_GID=`id -g`
-    docker-compose run php php -d memory_limit=512M bin/console scheduler:execute
+    docker-compose run php php -d memory_limit=512M bin/console scheduler:execute --env=${ENV}
 else
-    php bin/console scheduler:execute
+    php bin/console scheduler:execute --env=${ENV}
 fi
