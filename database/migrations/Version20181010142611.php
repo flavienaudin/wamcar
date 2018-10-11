cec4d1f7eb953355006c3171c7c3e0b761e8a860
Flavien Audin <flavien.audin@gmail.com>
2018-10-11 11:29:15 +0200

    Garage Multi-vendeur : Affectation des véhicules
    - correction : SQL Migration

--- database/migrations/Version20181010142611.php
+++ database/migrations/Version20181010142611.php
@@ -15,12 +15,13 @@ final class Version20181010142611 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE pro_vehicle ADD seller_id INT NOT NULL');
+        $this->addSql('ALTER TABLE pro_vehicle ADD seller_id INT DEFAULT NULL');
         $this->addSql('UPDATE pro_vehicle SET seller_id = (
                                 SELECT gu.pro_user_id as user_id
                                 from garage_pro_user gu
                                 where gu.role = \'GARAGE.ADMINISTRATOR\' and gu.garage_id = pro_vehicle.garage_id)'
         );
+        // $this->addSql('COMMIT');
         $this->addSql('ALTER TABLE pro_vehicle ADD CONSTRAINT FK_EE29225D8DE820D9 FOREIGN KEY (seller_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_EE29225D8DE820D9 ON pro_vehicle (seller_id)');
     }
