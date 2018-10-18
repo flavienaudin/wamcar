1ce0c26d0136127f2ee1b2e3ac3b171e7fbefc37
Flavien Audin <flavien.audin@gmail.com>
2018-10-18 10:57:58 +0200

    Configuration : adresses e-mail commerciaux et monitors. Mise à jour bundles

--- src/AppBundle/MailWorkflow/NotifyAdminOfGoogleApiReturnError.php
+++ src/AppBundle/MailWorkflow/NotifyAdminOfGoogleApiReturnError.php
@@ -3,19 +3,43 @@
 namespace AppBundle\MailWorkflow;
 
 
+use AppBundle\MailWorkflow\Model\EmailContact;
 use AppBundle\MailWorkflow\Model\EmailRecipientList;
+use AppBundle\MailWorkflow\Services\Mailer;
 use GoogleApi\Event\GoogleApiReturnErrorEvent;
 use GoogleApi\Event\GoogleApiReturnErrorEventHandler;
 use GoogleApi\Event\PlaceDetailError;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class NotifyAdminOfGoogleApiReturnError extends AbstractEmailEventHandler implements GoogleApiReturnErrorEventHandler
 {
+    /** @var array */
+    private $monitorsEmails;
+
+    /**
+     * NotifyAdminOfGoogleApiReturnError constructor.
+     * @param Mailer $mailer
+     * @param UrlGeneratorInterface $router
+     * @param EngineInterface $templating
+     * @param TranslatorInterface $translator
+     * @param string $type
+     * @param array $monitorsEmails
+     */
+    public function __construct(Mailer $mailer, UrlGeneratorInterface $router, EngineInterface $templating, TranslatorInterface $translator, string $type, array $monitorsEmails)
+    {
+        parent::__construct($mailer, $router, $templating, $translator, $type);;
+        $this->monitorsEmails = [];
+        foreach ($this->monitorsEmails as $monitorsEmail) {
+            $this->monitorsEmails[] = new EmailContact($monitorsEmail);
+        }
+    }
 
     public function notify(GoogleApiReturnErrorEvent $event)
     {
         $this->checkEventClass($event, PlaceDetailError::class);
 
-        $event->getCallParams();
         $this->send(
             $this->translator->trans('notifyAdminOfGoogleApiReturn.object', [], 'email'),
             'Mail/notifyAdminOfGoogleApiReturnError.html.twig',
@@ -24,8 +48,8 @@ class NotifyAdminOfGoogleApiReturnError extends AbstractEmailEventHandler implem
                 'message' => $event->getMessage(),
                 'callParams' => $event->getCallParams()
             ],
-            new EmailRecipientList(['wamcartest@gmail.com'])
-        );
 
+            new EmailRecipientList($this->monitorsEmails)
+        );
     }
 }
