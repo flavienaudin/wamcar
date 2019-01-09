050f3428f73d2091a53358a248669705b9aba7a9
Flavien Audin <flavien.audin@gmail.com>
2019-01-09 18:35:04 +0100

    URL Rewritting :
    - Annuaire : soumission du formulaire en POST + url de recherche par ville et champ libre

--- src/AppBundle/Controller/Front/ProContext/DirectoryController.php
+++ src/AppBundle/Controller/Front/ProContext/DirectoryController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Front\ProContext;
 
 
 use AppBundle\Controller\Front\BaseController;
+use AppBundle\Elasticsearch\Query\CityResultProvider;
 use AppBundle\Elasticsearch\Query\SearchResultProvider;
 use AppBundle\Form\DTO\SearchProDTO;
 use AppBundle\Form\Type\SearchProType;
@@ -20,27 +21,63 @@ class DirectoryController extends BaseController
     private $searchResultProvider;
     /** @var UserEditionService $userEditionService */
     private $userEditionService;
+    /** @var CityResultProvider */
+    private $cityResultProvider;
 
     /**
      * DirectoryController constructor.
      * @param FormFactoryInterface $formFactory
      * @param SearchResultProvider $searchResultProvider ,
      * @param UserEditionService $userEditionService
+     * @param CityResultProvider $cityResultProvider
      */
     public function __construct(
         FormFactoryInterface $formFactory,
         SearchResultProvider $searchResultProvider,
-        UserEditionService $userEditionService
+        UserEditionService $userEditionService,
+        CityResultProvider $cityResultProvider
     )
     {
         $this->formFactory = $formFactory;
         $this->searchResultProvider = $searchResultProvider;
         $this->userEditionService = $userEditionService;
+        $this->cityResultProvider = $cityResultProvider;
     }
 
     public function viewAction(Request $request, int $page = 1)
     {
+        // Normal use is query param. Attribute $page if for legacy purpose
+        $page = $request->query->get('page', $page);
+
         $searchProDTO = new SearchProDTO();
+
+        // Champ libre
+        if ($request->query->has('q')) {
+            $searchProDTO->text = $request->query->get('q');
+        }
+
+        // Deal with ByCity action
+        if (($city = $request->get('city')) !== null) {
+            $idxSplit = strpos($city, '-');
+            if ($idxSplit !== false) {
+                $cityPostalCode = substr($city, 0, $idxSplit);
+                $cityName = substr($city, $idxSplit + 1);
+                $cities = $this->cityResultProvider->provideForSearch($cityName);
+
+                if ($cities->totalHits() > 0) {
+                    foreach ($cities->hits() as $hit) {
+                        if ($hit['id'] === $cityPostalCode) {
+                            $searchProDTO->postalCode = $hit['id'];
+                            $searchProDTO->cityName = $hit['cityName'];
+                            $searchProDTO->latitude = $hit['latitude'];
+                            $searchProDTO->longitude = $hit['longitude'];
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
         $searchProForm = $this->formFactory->create(SearchProType::class, $searchProDTO, [
             'action' => $this->generateRoute('front_directory_view')
         ]);
