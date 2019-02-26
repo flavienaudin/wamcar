325c392c510b912cb611169ebb47994663459733
Flavien Audin <flavien.audin@gmail.com>
2019-02-26 09:31:49 +0100

    Inscription via HP : passer l'étape 'orientation' si question déjà posée

--- src/AppBundle/Controller/Front/SecurityController.php
+++ src/AppBundle/Controller/Front/SecurityController.php
@@ -145,16 +145,6 @@ class SecurityController extends BaseController
                 $registrationDTO = $registrationForm->getData();
                 $registrationDTO->type = $type;
                 $registeredUser = $this->userRegistrationService->registerUser($registrationDTO);
-
-                if ($registeredUser instanceof PersonalUser &&
-                    $this->session->has(RegistrationController::PERSONAL_ORIENTATION_ACTION_SESSION_KEY) &&
-                    PersonalOrientationChoices::isValidKey($this->session->get(RegistrationController::PERSONAL_ORIENTATION_ACTION_SESSION_KEY))) {
-                    // From landing mixte : orientation action is set in session
-                    $orientation = new PersonalOrientationChoices($this->session->get(RegistrationController::PERSONAL_ORIENTATION_ACTION_SESSION_KEY));
-                    $this->userEditionService->updateUserOrientation($registeredUser, $orientation);
-                    $this->session->remove(RegistrationController::PERSONAL_ORIENTATION_ACTION_SESSION_KEY);
-                }
-
             } catch (UniqueConstraintViolationException $exception) {
                 $this->session->getFlashBag()->add(
                     self::FLASH_LEVEL_DANGER,
@@ -163,6 +153,7 @@ class SecurityController extends BaseController
 
                 return $this->render(sprintf('front/Security/Register/user_%s.html.twig', $type), [
                     'form' => $registrationForm->createView(),
+                    'assitant_registration_mode' => (bool)$request->get('assistant_registration', false)
                 ]);
             }
 
