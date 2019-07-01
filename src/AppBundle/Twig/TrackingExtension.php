b2720d7936983d699c155ed264a0a045f3bf3c91
Flavien Audin <flavien.audin@gmail.com>
2019-07-01 16:41:11 +0200

    GTM Nomenclature : SC (Search Concseiller) / SA (Search Announce)

--- src/AppBundle/Twig/TrackingExtension.php
+++ src/AppBundle/Twig/TrackingExtension.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Twig;
 
 
+use Symfony\Component\Form\FormView;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -34,7 +35,9 @@ class TrackingExtension extends AbstractExtension
     {
         return [
             new TwigFunction('wtNoneValue', array($this, 'getWtNoneValue')),
-            new TwigFunction('wtLikeDataAttributes', array($this, 'getLikeWtDataAttributes'))
+            new TwigFunction('wtLikeDataAttributes', array($this, 'getLikeWtDataAttributes')),
+            new TwigFunction('wtSearchFormDataAttributes', array($this, 'getSearchFormWtDataAttributes')),
+            new TwigFunction('wtDirectorySearchFormDataAttributes', array($this, 'getDirectorySearchFormWtDataAttributes'))
         ];
     }
 
@@ -100,5 +103,82 @@ class TrackingExtension extends AbstractExtension
         }
         return ' data-wtaction="' . $action . ' ' . $vehicle->getSlug() . '" data-wtfrom="' . $this->getWtFromDataAttrValue($fromUser)
             . '" data-wtto="' . $this->getWtToDataAttrValue($toUser) . '"';
+    }
+
+    /**
+     * @param FormView $form
+     * @return array
+     */
+    public function getSearchFormWtDataAttributes(FormView $form, ?BaseUser $currentUser): array
+    {
+        if ($form->vars['submitted']) {
+            $attributes = ['data-wtaction' => 'SA', 'data-wtfrom' => $this->getWtFromDataAttrValue($currentUser)];
+            // text
+            if (!empty($text = $this->getFormElementValue($form, 'text'))) {
+                $attributes['data-wtrequest'] = $text;
+            }
+            // cityName, radius, postalCode  || latitude, longitude
+            if (!empty($city = $this->getFormElementValue($form, 'cityName'))) {
+                $attributes['data-wtcity'] = $city . '#' . $this->getFormElementValue($form, 'postalCode');
+                if (!empty($radius = $this->getFormElementValue($form, 'radius'))) {
+                    $attributes['data-wtradius'] = $radius;
+
+                }
+            }
+            // type
+            if (!empty($type = $this->getFormElementValue($form, 'type'))) {
+                $attributes['data-wttype'] = $type;
+            }
+            // sorting
+            if (!empty($sorting = $this->getFormElementValue($form, 'sorting'))) {
+                $attributes['data-wtsorting'] = $sorting;
+            }
+            /* make, model, transmission, fuel
+            mileageMax, yearsMin, yearsMax, budgetMin, budgetMax*/
+            return $attributes;
+        }
+        return [];
+    }
+
+    /**
+     * @param FormView $form
+     * @return array
+     */
+    public function getDirectorySearchFormWtDataAttributes(FormView $form, ?BaseUser $currentUser): array
+    {
+        if ($form->vars['submitted']) {
+            $attributes = ['data-wtaction' => 'SC', 'data-wtfrom' => $this->getWtFromDataAttrValue($currentUser)];
+            // text
+            if (!empty($text = $this->getFormElementValue($form, 'text'))) {
+                $attributes['data-wtrequest'] = $text;
+            }
+            // cityName, radius, postalCode  || latitude, longitude
+            if (!empty($city = $this->getFormElementValue($form, 'cityName'))) {
+                $attributes['data-wtcity'] = $city . '#' . $this->getFormElementValue($form, 'postalCode');
+                if (!empty($radius = $this->getFormElementValue($form, 'radius'))) {
+                    $attributes['data-wtradius'] = $radius;
+
+                }
+            }
+            // sorting
+            if (!empty($sorting = $this->getFormElementValue($form, 'sorting'))) {
+                $attributes['data-wtsorting'] = $sorting;
+            }
+
+            return $attributes;
+        }
+        return [];
+    }
+
+    private function getFormElementValue(FormView $form, string $name): ?string
+    {
+        if (isset($form->children[$name]) && !empty($form->children[$name]->vars['value'])) {
+            if (is_array($form->children[$name]->vars['value'])) {
+                return join(',', $form->children[$name]->vars['value']);
+            } else {
+                return $form->children[$name]->vars['value'];
+            }
+        }
+        return null;
     }
 }
