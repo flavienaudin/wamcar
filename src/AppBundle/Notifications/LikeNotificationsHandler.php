2d5cf6c9c2935eef35c97b0d77d2a1ddb2388e18
Flavien Audin <flavien.audin@gmail.com>
2019-07-19 11:16:57 +0200

    Fix : envoi e-mail immédiat après message

--- src/AppBundle/Notifications/LikeNotificationsHandler.php
+++ src/AppBundle/Notifications/LikeNotificationsHandler.php
@@ -82,7 +82,7 @@ class LikeNotificationsHandler extends AbstractEmailEventHandler implements Like
                     }
 
                     if ($vehicleSeller->getPreferences()->isLikeEmailEnabled() &&
-                        $vehicleSeller->getPreferences()->getLikeEmailFrequency()->getValue() === NotificationFrequency::IMMEDIATELY) {
+                        NotificationFrequency::IMMEDIATELY()->equals($vehicleSeller->getPreferences()->getLikeEmailFrequency())) {
 
                         $pathImg = $this->pathVehiclePicture->getPath($likedVehicle->getMainPicture(), 'vehicle_mini_thumbnail');
                         $trackingKeywords = ($vehicleSeller->isPro() ? 'advisor' : 'customer') . $vehicleSeller->getId();
@@ -107,7 +107,7 @@ class LikeNotificationsHandler extends AbstractEmailEventHandler implements Like
                                     'ea' => 'open',
                                     'el' => urlencode($emailObject),
                                     'dh' => $this->router->getContext()->getHost(),
-                                    'dp' => urlencode('/email/newlike/open/'.$like->getId()),
+                                    'dp' => urlencode('/email/newlike/open/' . $like->getId()),
                                     'dt' => urlencode($emailObject),
                                     'cs' => 'notifications', // Campaign source
                                     'cm' => 'email', // Campaign medium
