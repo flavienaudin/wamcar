694494353bff7e8854a1dcfb23ff2b1a92f284f6
Flavien Audin <flavien@wamcar.com>
2020-09-30 14:28:38 +0200

    Suppr new searchVehicleDTO

--- src/AppBundle/Controller/Front/ProContext/DirectoryController.php
+++ src/AppBundle/Controller/Front/ProContext/DirectoryController.php
@@ -176,7 +176,6 @@ class DirectoryController extends BaseController
 
         $proUsersResultSet = $this->proUserEntityIndexer->getQueryDirectoryProUserResult($searchProDTO, $page, $this->getUser());
         $proUserResult = $this->userEditionService->getUsersBySearchResult($proUsersResultSet);
-        $searchVehicleDTO = new SearchVehicleDTO();
         $searchVehiclesResultSet = $this->searchResultProvider->getSearchResult($searchVehicleDTO, $page, $this->getUser(), 4);
         $searchVehiclesItems = $this->userEditionService->getMixedBySearchItemResult($searchVehiclesResultSet);
 
